df66e158ea2f21950816363d058a6ab78fb57de2
joaofelipe <joaofelipe1294@gmail.com>
2021-09-12 04:12:31 -0300

    feat: implemented first_and_last_name.

--- lib/student.ex
+++ lib/student.ex
@@ -1,4 +1,45 @@
-defmodule Student
+defmodule Student do
+  @moduledoc """
+    Define sturct student and a function used to return only first and last name.
+  """
+
   @enforce_keys [:name]
   defstruct name: nil, results: []
+
+  @doc """
+    Reduce student name only to first and last names
+
+    ## Parameters
+
+    - complete_name: student full name
+  """
+  def first_and_last_name(complete_name) do
+    first_name = first_name(complete_name)
+    last_name = last_name(complete_name)
+    "#{first_name} #{last_name}"
+  end
+
+  @doc """
+    Return student first name
+
+    ## Parameters
+
+    - complete_name: name separated by spaces
+  """
+  defp first_name(complete_name) do
+    String.split(complete_name, " ")
+    |> hd
+  end
+
+  @doc """
+    Return student last name
+
+    ## Parameters
+
+    - complete_name: name separated by spaces
+  """
+  defp last_name(complete_name) do
+    String.split(complete_name, " ")
+    |> List.last()
+  end
 end
